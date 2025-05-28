10c000d93670493d3e598e22127c5080c9db87a3
mcb237 <mc2370@cam.ac.uk>
2025-05-28 21:51:01 +0100

    comments on decoder.py

--- decoder.py
+++ decoder.py
@@ -10,6 +10,7 @@ def decode(signal: np.ndarray, filter: np.ndarray) -> np.ndarray:
     Decode the received signal into symbols.
     """
     fourier = np.fft.fft(signal, axis=1) * filter
+    # Apply high-pass filter to remove problematic frequencies
     symbols = fourier[:, 1 + HIGH_PASS_INDEX: 1 + HIGH_PASS_INDEX + SYMBOLS_PER_BLOCK].flatten()
     return symbols
 
@@ -18,14 +19,18 @@ def synchronize(signal: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     Synchronize the received signal and return the filter.
     """
     chirp_signal = get_chirp()
+    # Find start and end indices of the signal by correlation with the chirp
     startIndex = np.argmax(np.correlate(signal, chirp_signal[::-1]))
     endIndex = np.argmax(np.correlate(signal, chirp_signal))
     
     known_blocks = get_known_blocks()
 
+    # TODO: make frame length variable 
     frameLength = 2 * BLOCK_LENGTH
+    # left_bound is the start of the first data block
     left_bound = startIndex + CHIRP_LENGTH - frameLength//2
 
+    # sync for each data block using correlation of each known block
     sync_indices = np.array([])
     current_index = 0
     while left_bound + frameLength < endIndex:
@@ -35,16 +40,21 @@ def synchronize(signal: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
 
         current_index += 1
 
-    
+    # indices are the theoretical start indices of each data block - found by adding the frame length to the start index of the first data block
     indices = np.arange(current_index) * frameLength + startIndex + CHIRP_LENGTH
+    # Find the drift gradient by regression of the theoretical indices and the indices found by synchronization.
+    # The mean is subtracted to make the y intercept zero so that this formula can be used.
     drift_gradient = np.dot(indices - np.mean(indices), sync_indices - np.mean(sync_indices)) / np.sum((indices - np.mean(indices)) ** 2)
+    # What's the drift constant for? Is the regression not zero intercept?
     drift_constant = np.mean(sync_indices) - drift_gradient * np.mean(indices)
 
+    # TODO: Need further explantation on this
     frame_indices = (np.arange(startIndex + CHIRP_LENGTH, startIndex + CHIRP_LENGTH + current_index * frameLength) * drift_gradient + drift_constant).reshape(-1, frameLength)
     extracted_indices = np.vstack([np.arange(round(row[0]), round(row[0]) + frameLength) for row in frame_indices])
     drift = extracted_indices - frame_indices
     received_blocks = signal[extracted_indices]
 
+    # get the known blocks and informations blocks and their drift
     sent_known_blocks = known_blocks[:, CYCLIC_PREFIX:BLOCK_LENGTH]
     known_block_drift = drift[:, CYCLIC_PREFIX:BLOCK_LENGTH]
     received_known_blocks = received_blocks[:, CYCLIC_PREFIX:BLOCK_LENGTH]
@@ -65,11 +75,18 @@ def estimate_filter(sent_known_blocks: np.ndarray,
     """
     bins = BLOCK_LENGTH - CYCLIC_PREFIX
 
+    # FFT of sent and received known blocks
     sent_fourier = np.fft.fft(sent_known_blocks, axis=1)
     received_fourier = np.fft.fft(received_known_blocks, axis=1)
+
     received_fourier *= np.exp(-2j * np.pi * np.arange(bins) * known_block_drift / bins)
+    # variable name 'zero_forcing_filter' is misleading, it should be channel_coefficient_estimate or similar instead
+    # Zero forcing filter is the inverse of the channel coefficient estimate
     zero_forcing_filter = received_fourier / (sent_fourier + 1e-10)
+    # TODO: snr to be an array since it is different for each bin
+    # TODO: this requires a coarse channel coefficient estimate which can then be used in Y = X * H + N to get noise variance for better snr estimate
     filter = np.conjugate(zero_forcing_filter) / (np.abs(zero_forcing_filter) ** 2 + 1 / snr)
+    # TODO: need further explanation of this rotation
     filter = np.mean(filter, axis=0) * np.exp(-2j * np.pi * np.arange(bins) * information_block_drift / bins)
     return filter
 
